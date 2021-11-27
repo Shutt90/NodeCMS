89e40dd89b3d51c7707b1098fc6ed21e7e41a060
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-11-27 19:21:47 +0000

    Working login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,7 @@ const register_store = async (req, res) => {
         const user = new User({
             first_name: req.body.first_name,
             surname: req.body.surname,
+            email: req.body.email,
             username: req.body.username,
             addnumber: req.body.addnumber,
             addstreet: req.body.addstreet,
@@ -27,8 +28,45 @@ const register_store = async (req, res) => {
     } 
 }
 
+const login_index = (req, res) => {
+    res.render('auth/login', {title: 'login'})
+}
+
+const login_auth = async (req, res) => {
+
+    const email = req.body.email;
+    const user = User.findOne({
+        email: email,
+    })
+
+    const password = await bcrypt.compare(req.body.password, user.password, async function(res, err) {
+        if(err) {
+            console.log(err)
+        }
+
+    });
+
+    const userFound = User.findOne({
+        email: email,
+        password: password,
+    })
+
+    try {
+        if(!userFound) {
+            return res.status(400).send("We don't have a user with that email")
+        } else {
+            res.status(200).send('Success')
+        }
+
+    } catch(error) {
+        res.status(500).send('Server Error');
+    }
+}
+
 
 module.exports = {
     register_index,
     register_store,
+    login_index,
+    login_auth,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,21 +3,14 @@ const router = express.Router();
 
 const authController = require('../controllers/authController');
 
-
-router.get('/auth', (req, res) => {
-    res.render('auth/login', {title: 'Login'});
-})
-
 router.get('/register', authController.register_index)
 
 router.post('/register', authController.register_store)
 
-router.post('/login', (req, res, next) => {
+router.get('/login', authController.login_index)
 
-})
+router.post('/login', authController.login_auth)
 
-router.get('/logout', (req, res) => {
-    
-})
+// router.get('/logout', authControll.login_auth)
 
 module.exports = router
