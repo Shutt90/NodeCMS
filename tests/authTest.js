bfe0f48ac6795862684748ab113ccd30c9f78324
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-12-03 19:21:37 +0000

    Refactored to secrets

--- tests/authTest.js
+++ tests/authTest.js
@@ -1,9 +1,9 @@
 const assert = require('assert')
 const authController = require('../controllers/authController')
 const mongoose = require('mongoose');
+const secrets = require('../secrets');
 
-const con = 'mongodb+srv://tester:zP1Ucfu0RYDeiwgr@nodecms.bp90q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+const con = secrets.db;
 
 mongoose.connect(con)
     .then(async function tests() {
@@ -11,7 +11,7 @@ mongoose.connect(con)
         await test2();
 
         async function test1() {
-            const email = 'Liam.pugh.009@gmail.com'
+            const email = secrets.email
 
             const result = await authController.test_auth(email)
             assert.deepEqual(result.email, email)
@@ -19,7 +19,7 @@ mongoose.connect(con)
 
         //Takes 2 arguements, email, password
         async function test2() {
-            const something = await authController.check_user('Liam.pugh.009@gmail.com', 'password')
+            const something = await authController.check_user(secrets.email, secrets.fakePassword)
             assert.equal(something, true)
 
         }
