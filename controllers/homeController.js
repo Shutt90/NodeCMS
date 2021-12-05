625b438a3d7db5396725c1b16daebef80a2790bd
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-12-05 21:38:01 +0000

    Nav working on dynamic pages

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -19,20 +19,23 @@ const home_index = async (req, res) => {
 
 const home_dynamic = async (req, res) => {
     try {
-        
+
         let name = req.params.title;
-        console.log(name);
 
         const currentPage = await Page.findOne({anchor: name})
+        const pages = await Page.find()
+
 
         res.render('dynamic', {
             title: currentPage.anchor,
             page: currentPage,
+            pages: pages,
         })
 
     } catch(err) {
         console.error(err)
     }
+
 
 }
 
