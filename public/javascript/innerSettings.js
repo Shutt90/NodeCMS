bae83429d8aa1de188a29c4310462bfb43cd45e7
Liam Pugh <liam.pugh.009@gmail.com>
2021-12-21 21:34:32 +0000

    Added profile tab innerhtml

--- public/javascript/innerSettings.js
+++ public/javascript/innerSettings.js
@@ -13,10 +13,34 @@ const socialsHtml = `
         <label for="linkedin">LinkedIn:</label>
         <input type="text" name="linkedin" id="linkedin">
         <input class="submit-btn" type="submit">   
+    </form>`
+
+const profileHtml = `
+    <form class="inner-tabs" method="" action="POST">
+        <label for="first_name">First Name:</label>
+        <input type="text" name="first_name" id="first_name">
+        <label for="surname">Surname:</label>
+        <input type="text" name="surname" id="surname">
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+        <label for="contact">Contact:</label>
+        <input type="text" name="contact" id="contact">
+        <label for="old_password">Old Password:</label>
+        <input type="password" name="old_password" id="old_password">
+        <label for="new_password">New Password:</label>
+        <input type="password" name="new_password" id="new_password">
+        <label for="new_password_confirmation">New Password Confirmed:</label>
+        <input type="password" name="new_password_confirmation" id="new_password_confirmation">
+        <input class="submit-btn" type="submit">   
     </form>
+
 `
 
 
 socialsTab.addEventListener('click', function() {
     getInner.innerHTML = socialsHtml
+})
+
+profileTab.addEventListener('click', function() {
+    getInner.innerHTML = profileHtml
 })
